0b1954ca579eddc4ad44be183b3548a797773535
HYUNJI0624 <80306575+HYUNJI0624@users.noreply.github.com>
2024-01-15 20:03:05 +0900

    feat: create a label component

---
+++ src/components/Label/MessageLabel.jsx
@@ -0,0 +1,9 @@
+import Label from "../common/label";
+
+export const MessageLabel = ({ text }) => {
+  return (
+    <Label fontSizeType={"smallFont"} backgroundColor="white">
+      {text}
+    </Label>
+  );
+};
---
+++ src/components/common/Label.jsx
@@ -0,0 +1,46 @@
+import styled, { css } from "styled-components";
+
+const Label = ({
+  fontSizeType,
+  backgroundColor = "transparent",
+  children,
+  ...props
+}) => {
+  return (
+    <StyledLabel
+      fontSizeType={fontSizeType}
+      backgroundColor={backgroundColor}
+      {...props}
+    >
+      {children}
+    </StyledLabel>
+  );
+};
+
+const StyledLabel = styled.label`
+  ${({ fontSizeType }) => FontSize[fontSizeType]}
+  ${({ backgroundColor }) =>
+    css`
+      background-color: ${backgroundColor};
+    `}
+`;
+
+const SmallFontSize = css`
+  font-size: 14px;
+`;
+
+const MediumFontSize = css`
+  font-size: 18px;
+`;
+
+const LargeFontSize = css`
+  font-size: 30px;
+`;
+
+const FontSize = {
+  smallFont: SmallFontSize,
+  mediumFont: MediumFontSize,
+  largeFont: LargeFontSize,
+};
+
+export default Label;
