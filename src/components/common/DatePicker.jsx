06907bc3c0cb1982a95019e9a20406a29a7b959b
HYUNJI0624 <80306575+HYUNJI0624@users.noreply.github.com>
2024-01-19 22:49:52 +0900

    feat: changing a date picker component's package

--- src/components/common/DatePicker.jsx
+++ src/components/common/DatePicker.jsx
@@ -1,23 +1,18 @@
 import { useState } from "react";
-import ReactDatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
-import { ko } from "date-fns/esm/locale";
-import styled from "styled-components";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker as ReactDatePicker } from "@mui/x-date-pickers/DatePicker";
+import dayjs from "dayjs";
 
 export const DatePicker = () => {
-  const [startDate, setStartDate] = useState(new Date());
-
+  const [startDate, setStartDate] = useState(dayjs());
   return (
-    <StyledDatePicker
-      locale={ko}
-      selected={startDate}
-      onChange={(date) => setStartDate(date)}
-      dateFormat="yyyy.MM.dd E요일"
-    />
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <ReactDatePicker
+        format="YYYY. MM. DD. ddd"
+        value={startDate}
+        onChange={(date) => setStartDate(date)}
+      />
+    </LocalizationProvider>
   );
 };
-
-const StyledDatePicker = styled(ReactDatePicker)`
-  width: 110px;
-  padding: 5px 10px;
-`;
