692dea0c03169d4d5673a58fc0b13cf82e640df2
Hemudi <ksum1205@naver.com>
2024-01-23 07:11:10 +0900

    feat: add ThemeProvider to app and storybook

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { ThemeProvider } from "styled-components";
 import GlobalStyle from "./styles/GlobalStyle";
+import theme from "./styles/theme";
 
 const App = () => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
-    </>
+    </ThemeProvider>
   );
 };
 
