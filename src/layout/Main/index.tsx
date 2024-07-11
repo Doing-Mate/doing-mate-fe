36280346278c5102aa20a56fbadd923b751e8b6c
Hemudi <ksum1205@naver.com>
2024-07-11 15:19:19 +0900

    fix: fix scheduleList updates trigger UI re-render

--- src/layout/Main/index.tsx
+++ src/layout/Main/index.tsx
@@ -5,50 +5,38 @@ import { useModalStateContext } from "../../hooks/useModalStateContext";
 import { useModalModeContext } from "../../hooks/useModalModeContext";
 import { useScheduleDataContext } from "../../hooks/useScheduleDataContext";
 import { useCategoryDataContext } from "../../hooks/useCategoryDataContext";
-import {
-  ScheduleDetailComponent,
-  ScheduleAddComponent,
-} from "../../context/modalPageComponents";
+import { ScheduleDetailComponent, ScheduleAddComponent } from "../../context/modalPageComponents";
 import { getScheduleData, getCategoryData } from "../../apis/fetch";
-import { ScheduleDataProps } from "../../context/dataInterface";
 
 export const Main = () => {
   const { onModal, setOnModal } = useModalStateContext();
   const { setModalMode } = useModalModeContext();
   const { scheduleList, setScheduleList } = useScheduleDataContext();
-  const { categoryList, setCategoryList } = useCategoryDataContext();
+  const { setCategoryList } = useCategoryDataContext();
 
   const getAndSetDataList = async () => {
-    setScheduleList(await getScheduleData());
-    setCategoryList(await getCategoryData());
+    const categoryData = await getCategoryData();
+    const scheduleData = await getScheduleData();
+    const scheduleList = scheduleData.map((scheduleItem) => {
+      const category = categoryData.find((categoryItem) => categoryItem.id === scheduleItem.category_id);
+      return {
+        ...scheduleItem,
+        color: category?.color || scheduleItem.color,
+      };
+    });
+
+    setCategoryList(categoryData);
+    setScheduleList(scheduleList);
   };
 
   useEffect(() => {
     getAndSetDataList();
   }, []);
 
-  useEffect(() => {
-    const newScheduleList: ScheduleDataProps[] = scheduleList.map(
-      (scheduleItem) => {
-        scheduleItem.color = categoryList.find(
-          (categoryItem) => categoryItem.id === scheduleItem.category_id
-        )?.color;
-        return scheduleItem;
-      }
-    );
-    setScheduleList(newScheduleList);
-  }, [scheduleList, categoryList, setScheduleList]);
-
-  useEffect(() => {
-    console.log(scheduleList);
-  }, [scheduleList]);
-
   const eventClick = (info: EventClickArg) => {
     const data = info.event._def;
 
-    ScheduleDetailComponent.data = scheduleList.find(
-      (scheduleItem) => scheduleItem.id === data.publicId
-    );
+    ScheduleDetailComponent.data = scheduleList.find((scheduleItem) => scheduleItem.id === data.publicId);
 
     setModalMode(ScheduleDetailComponent);
     setOnModal(!onModal);
@@ -59,11 +47,5 @@ export const Main = () => {
     setOnModal(!onModal);
   };
 
-  return (
-    <Calendar
-      events={scheduleList}
-      eventClick={eventClick}
-      todoPlusClick={todoPlusClick}
-    />
-  );
+  return <Calendar events={scheduleList} eventClick={eventClick} todoPlusClick={todoPlusClick} />;
 };
